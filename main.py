d1ff023c6c6fcafccc1a00a8fa14ec4d3aaaa952
kulvirvirk <kvirkdev@gmail.com>
2020-04-03 05:07:00 +0000

    generated random number between 1 to 10

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 # 1. ask the players name
 # 2. 
 
-
+import random
 player_name = input('Hello, what is your name? ')
 
 print('Hi ' + player_name + '!')
 
+#generate random number
+random_number = random.randrange(1, 11)
+
+player_guess = input('I am thinking of a nubmer between 1 and 20. Take a guess: ')
+
+print(random_number)
 
